df2c661b7c5ef66793cedb6475d8b4816951968b
toniartemenko <toni.artemenko2@gmail.com>
2017-08-30 18:32:40 +0300

    change

--- webpack.config.js
+++ webpack.config.js
@@ -4,12 +4,23 @@ module.exports = {
     'webpack/hot/only-dev-server',
     './src/index.js'
   ],
+  module: {
+    loaders: [{
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      loader: "babel-loader"
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
   output: {
     path: __dirname + '/dist',
     publicPath: '/',
     filename: 'bundle.js'
   },
   devServer: {
-    contentBase: './dist'
+    contentBase: './dist',
+    hot: true
   }
 };
